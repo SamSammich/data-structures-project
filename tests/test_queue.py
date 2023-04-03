3af73ca05714c9fc44ce5413b466cbb2b91913d3
Sam <Abdusamadov55@gmail.com>
2023-04-03 14:21:37 +0900

    Not fully Done Homework 3 (test coverage 99%)

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -4,13 +4,14 @@ from src import queue
 
 
 class TestQueue(unittest.TestCase):
+    """Testing func. engueue"""
     def test_enqueue(self):
         test_queue = queue.Queue()
         test_queue.enqueue('data1')
         test_queue.enqueue('data2')
         self.assertEqual(test_queue.head.data, 'data1')
         self.assertEqual(test_queue.head.next_node.data, 'data2')
-
+        """Testing func. queue"""
     def test_empty_queue(self):
         test_queue = queue.Queue()
         test_queue.enqueue('data1')
@@ -18,3 +19,4 @@ class TestQueue(unittest.TestCase):
         self.assertEqual(test_queue.dequeue(), 'data1')
         test_queue.dequeue()
         self.assertIsNone(test_queue.dequeue())
+
